838a31a425f072e7d2c05588cb8d6ffcc7b013a2
Patrick Calahan <github@pcal.net>
2023-09-14 11:41:19 -0700

    checkpoint

--- src/main/java/net/pcal/copperhopper/mixins/MinecartItemDispenserBehaviorMixin.java
+++ src/main/java/net/pcal/copperhopper/mixins/MinecartItemDispenserBehaviorMixin.java
@@ -1,8 +1,10 @@
 package net.pcal.copperhopper.mixins;
 
+import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity.Type;
-import net.minecraft.item.MinecartItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPointer;
 import net.minecraft.world.World;
 import net.pcal.copperhopper.CopperHopperMinecartEntity;
 import net.pcal.copperhopper.CopperHopperMinecartItem;
@@ -11,20 +13,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
- * Ensure CHMs get placed correctly.
+ * Ensure CHMs get dispensed correctly.
  *
  * @author pcal
  * @since 0.5.0
  */
-@Mixin(MinecartItem.class)
-public abstract class MinecartItemMixin {
+@Mixin(targets = "net/minecraft/item/MinecartItem$1")
+public abstract class MinecartItemDispenserBehaviorMixin {
 
-    //FIXME repeat for dispenseSilently
-
-    @Redirect(method = "useOnBlock",
+    @Redirect(method = "dispenseSilently(Lnet/minecraft/util/math/BlockPointer;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;",
             at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;create(Lnet/minecraft/world/World;DDDLnet/minecraft/entity/vehicle/AbstractMinecartEntity$Type;)Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;"))
-    private AbstractMinecartEntity coho__createMinecart(World world, double x, double y, double z, Type type) {
-        if (((Object)this) instanceof CopperHopperMinecartItem) {
+    private AbstractMinecartEntity coho__createMinecart(World world, double x, double y, double z, Type type, BlockPointer pointer, ItemStack stack) {
+        if (stack.getItem() instanceof CopperHopperMinecartItem) {
             return new CopperHopperMinecartEntity(world, x, y, z);
         } else {
             return AbstractMinecartEntity.create(world, x, y, z, type);
