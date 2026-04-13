e530c408a81ea85bd41fe1db2cfe486e843cf3f9
Patrick Calahan <github@pcal.net>
2026-04-13 12:55:28 -0700

    fix import

--- common/src/main/java/net/pcal/copperhopper/common/CohoBlock.java
+++ common/src/main/java/net/pcal/copperhopper/common/CohoBlock.java
@@ -37,6 +37,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HopperBlock;
 import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
